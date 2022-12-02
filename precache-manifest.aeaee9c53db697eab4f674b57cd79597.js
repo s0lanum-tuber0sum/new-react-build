238d83eda9b4a415701f10f65849d7e7fe33b468
s0lanum-tuber0sum <itirmkitchen@gmail.com>
2022-12-02 22:37:37 +0530

    asdas

---
+++ precache-manifest.aeaee9c53db697eab4f674b57cd79597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfe4169b203394aa7e76",
+    "url": "/static/css/main.36ecb4ac.chunk.css"
+  },
+  {
+    "revision": "cfe4169b203394aa7e76",
+    "url": "/static/js/main.cfe4169b.chunk.js"
+  },
+  {
+    "revision": "77714e258c802c79d866",
+    "url": "/static/js/1.77714e25.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6820f5616d7cdf2f26e34bc6b9c43f4d",
+    "url": "/index.html"
+  }
+];
